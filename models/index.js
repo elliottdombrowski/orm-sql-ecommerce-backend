99eb7ed79bb7db2f5f6700a67d986e3009692a33
elliottdombrowski <elliottroyal@me.com>
2021-10-21 15:17:33 -0500

    Playing with category routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+}),
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+}),
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,15 +6,43 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+      
+    if (!categoryData) {
+      res.status(404).json({ message: 'No ID found.' });
+      return;
+    }
+        
+    res.status(200).json(categoryData);   
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  let newCategory = {
+    // title: req.params.title,
+    // text: req.params.text,
+    // id: ???
+  }
 });
 
 router.put('/:id', (req, res) => {
